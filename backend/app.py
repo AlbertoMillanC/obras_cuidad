6eebc6bd46bc2806e222d750bf79872b10026851
AlbertoMillánC <carlosalbertomillanc@gmail.com>
2025-04-14 22:07:19 -0500

    denotado

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///obras.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/obras.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
